d7976791e5d3965f3643e5f179c50622e48f5234
PerturbatorAndTonny <perturbatorandtonny@gmail.com>
2025-10-02 20:33:32 -0500

    cambios postman

--- src/routes/animal.js
+++ src/routes/animal.js
@@ -2,11 +2,19 @@ const express = require("express");
 const router = express.Router(); //manejador de rutas de express
 const animalSchema = require("../models/animalModel");
 //Nuevo animal
-router.post("/animals", (req, res) => {
+router.post("/animalitos", (req, res) => {
     const animal = animalSchema(req.body);
     animal
         .save()
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
+
+//Consultar todos los animales
+router.get("/animalitos", (req, res) => {
+    animalSchema.find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
 module.exports = router;

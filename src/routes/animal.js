767e8ec84373ec2f87095ae254a476094b2dca9b
Alejandraqt <alejandradurannq@gmail.com>
2025-10-16 20:28:18 -0500

    Creacion usuario y encriptacion claves

--- src/routes/animal.js
+++ src/routes/animal.js
@@ -12,9 +12,10 @@ router.post("/animalitos", (req, res) => {
 
 //Consultar todos los animales
 router.get("/animalitos", (req, res) => {
-    animalSchema.find()
+    animalSchema.find({edad:{$eq:4}})
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
 
+//animalSchema.find({edad:{$gt:2}}) ---> {edad:{$gt:2} es eq a select * from animals where edad > 2
 module.exports = router;
